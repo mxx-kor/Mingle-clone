2da61457829c2d386cbadf35dc5ab702a093d7aa
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-06-25 06:15:39 +0900

    tailwind 설치 및 설정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
-import "./App.css"
 
 const App = () => {
     return (
         <div>
-            <h1>
+            <h1 className="text-center mt-[230px]">
                 <span>M</span><span>i</span><span>n</span><span>g</span><span>l</span><span>e</span>
             </h1> 
             <form action="https://www.google.com/search" method="GET">
                 <div>
-                    <input name="q" type="text" placeholder="Mingle search 또는 URL입력" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                    <input className="ml-6" name="q" type="text" placeholder="Mingle search 또는 URL입력" aria-label="Recipient's username" aria-describedby="button-addon2" />
                 </div>
             </form>
         </div>
