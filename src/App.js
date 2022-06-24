34c40503d02bff3d0eb6b3a7c69dd6e58e5bcad9
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-06-25 06:53:56 +0900

    set dark mode
    navbar, footer, routes 컴포넌트 생성
    tailwind 활용 다크모드 설정법 학습

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navbar } from './components/Navbar'
+import { Footer } from './components/Footer'
+import { Routes } from './components/Routes'
 
 const App = () => {
+    const [darkTheme, setDarkTheme] = useState(false);
+
     return (
-        <div>
-            <h1 className="text-center mt-[230px]">
-                <span>M</span><span>i</span><span>n</span><span>g</span><span>l</span><span>e</span>
-            </h1> 
-            <form action="https://www.google.com/search" method="GET">
+        <div className={darkTheme ? 'dark' : ''}>
+            <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200 black min-h-screen">
+                <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
                 <div>
-                    <input className="ml-6" name="q" type="text" placeholder="Mingle search 또는 URL입력" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                    <h1 className="text-center mt-[230px]">
+                        <span>M</span><span>i</span><span>n</span><span>g</span><span>l</span><span>e</span>
+                    </h1> 
+                    <form action="https://www.google.com/search" method="GET">
+                        <div>
+                            <input className="ml-6" name="q" type="text" placeholder="Mingle search 또는 URL입력" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                        </div>
+                    </form>
                 </div>
-            </form>
+                <Footer />
+                <Routes />
+            </div>
         </div>
     )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export function Navbar({ darkTheme, setDarkTheme }) {
+  return (
+    <div className='p-5 pb-0 flex flex-wrap sm:justify-between justify-center border-b dark:border-gray-700 border-gray-200'>
+      <div className='flex justify-between items-center space-x-5 w-screen'>
+        <Link to='/'>
+          <p className='text-2xl'>
+            Mingle
+          </p>
+        </Link>
+        <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-900 dark:text-gray-200 bg-white border rounded-full px-2 py-1 hover:shadow-lg">
+          {darkTheme ? '💡 Light' : '🌙 Dark'}
+        </button>
+      </div>
+    </div>
+  )
+}
