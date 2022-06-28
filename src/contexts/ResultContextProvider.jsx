f0f102c0d423fb10d7c6add85ddfdecc70f6bdde
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-06-28 18:17:14 +0900

    에러 해결
    자바스크립트에 entries라는 내장함수가 있었고 이를 불러오기 위해 pathname을 이용해 같은 results 변수를 사용할 수 있도록함

--- src/contexts/ResultContextProvider.jsx
+++ src/contexts/ResultContextProvider.jsx
@@ -6,7 +6,7 @@ const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
 export const ResultContextProvider = ({ children }) => {
     const [results, setResults] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [searchTerm, setSearchTerm] = useState('');
+    const [searchTerm, setSearchTerm] = useState('Tesla');
 
     const getResults = async (type) => {
         setIsLoading(true);
@@ -22,9 +22,15 @@ export const ResultContextProvider = ({ children }) => {
         });
 
         const data = await response.json();
-        console.log(data);
 
-        setResults(data);
+        if(type.includes('/news')) {
+            setResults(data.entries);
+        } else if (type.includes('/image')) {
+            setResults(data.image_results);
+        } else {
+            setResults(data.results);
+        }
+        
         setIsLoading(false);
     }
 
