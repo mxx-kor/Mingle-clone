7831f7e424b2e650f5bd3503d98c78bd827491b6
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-06-28 18:15:00 +0900

    Update Routes.jsx
    키값을 설정하지 않아 오류 발생, 수정 완료

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -8,8 +8,8 @@ export function Routes() {
     <div className='p-4'>
       <Switch>
         <Route exact path='/' element={<Navigate replace to="/search" />} />
-        {['/search', '/images', '/news', '/videos'].map(path => (
-          <Route exact path={path} element={<Results />} />
+        {['/search', '/image', '/news', '/video'].map((path, index) => (
+          <Route exact path={path} key={index} element={<Results />} />
         ))}
       </Switch>
     </div>
