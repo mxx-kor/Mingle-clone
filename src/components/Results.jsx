ed8a71b471f1bc8c9c1186b58d8617d645d897f6
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-06-28 18:16:19 +0900

    image & news 검색결과 표시 기능
    useLocation pathname을 이용해 url을 전달하도록함, 각 파라미터를 표시할 수 있도록함

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import { useLocation } from 'react-router-dom';
 import ReactPlayer from 'react-player';
 
@@ -10,18 +10,22 @@ export const Results = () => {
   const location = useLocation();
 
   useEffect(() => {
-    getResults('/search/q=JavaScrpt Mastery&num=40')
-  }, [])
+    if (searchTerm) {
+      if(location.pathname == "/video") {
+        getResults(`/search/q=${searchTerm} video`)
+      } else {
+        getResults(`${location.pathname}/q=${searchTerm}&num=40`)
+      }
+    }
+  }, [searchTerm, location.pathname]);
 
   if(isLoading) return <Loading />;
-
-  console.log(location.pathname)
 
   switch (location.pathname) {
     case '/search':
       return (
         <div className='flex flex-wrap justify-between space-y-6 sm:px-56'>
-          {results?.results?.map(({ link, title }, index)=> (
+          {results?.map(({ link, title }, index)=> (
             <div key={index} className="md:w-2/5 w-full">
               <a href={link} target="_blank" rel='noreferrer'>
                 <p className='text-sm'>
@@ -33,14 +37,41 @@ export const Results = () => {
               </a>
             </div>
           ))}
-
         </div>
       );
-    case '/images':
-      return 'SEARCH';
+    case '/image':
+      return (
+        <div className='flex flex-wrap justify-center items-center'>
+          {results?.map(({ image, link: { href, title }}, index)=> (
+            <a className='sm:p-3 p-5' href={href} key={index} target="_blank" rel='noreferrer'>
+              <img src={image?.src} alt={title} loading="lazy" />
+              <p className='w-36 break-words text-sm mt-2'>
+                {title}
+              </p>
+            </a>
+          ))}
+        </div>
+      );
     case '/news':
-      return 'SEARCH';
-    case '/videos':
+      return (
+        <div className='flex flex-wrap justify-between space-y-6 sm:px-56 items-center'>
+          {results?.map(({ links, id, source, title })=> (
+            <div key={id} className="md:w-2/5 w-full">
+              <a href={links?.[0].href} target="_blank" rel='noreferrer' className='hover:underline'>
+                <p className='text-lg dark:text-blue-300 text-blue-700'>
+                  {title}
+                </p>
+              </a>
+              <div className='flex gap-4'>
+                <a href={source?.href} target="_blank" rel='noreferrer'>
+                  {source?.href}
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      );
+    case '/video':
       return 'SEARCH'; 
   
     default:
