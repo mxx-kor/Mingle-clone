bbc95086e2bd3f44c96982a056fc5835474f574b
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-06-29 16:55:30 +0900

    useCallback 활용 오류 해결(이슈에 정리)

--- src/contexts/ResultContextProvider.jsx
+++ src/contexts/ResultContextProvider.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useCallback, useContext, useState } from "react";
 
 const ResultContext = createContext();
 const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
@@ -6,9 +6,9 @@ const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
 export const ResultContextProvider = ({ children }) => {
     const [results, setResults] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [searchTerm, setSearchTerm] = useState('Tesla');
+    const [searchTerm, setSearchTerm] = useState('Elon Musk');
 
-    const getResults = async (type) => {
+    const getResults = useCallback(async (type) => {
         setIsLoading(true);
 
         const response = await fetch(`${baseUrl}${type}`, {
@@ -32,7 +32,7 @@ export const ResultContextProvider = ({ children }) => {
         }
         
         setIsLoading(false);
-    }
+    }, []);
 
     return (
         <ResultContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}>
