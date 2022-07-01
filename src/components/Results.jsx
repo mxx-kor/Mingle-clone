5b64365a6c85ef7be8f862dbc1a49b5038a8a201
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-07-01 15:29:15 +0900

    2가지 에러 해결

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -29,7 +29,7 @@ export const Results = () => {
             <div key={index} className="md:w-2/5 w-full">
               <a href={link} target="_blank" rel='noreferrer'>
                 <p className='text-sm'>
-                  {link.length > 30 ? link.substring(0, 30) : link}
+                  {link.length > 30 ? link.substring(0, 30) : JSON.stringify(link)}
                 </p>
                 <p className='text-lg hover:underline dark:text-blue-300 text-blue-700'>
                   {title}
