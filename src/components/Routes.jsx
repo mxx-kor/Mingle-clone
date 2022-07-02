fc3ba90c9f591ce6d2ff61c27aaea4423990224a
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-07-03 03:56:01 +0900

    main 페이지 작업 준비

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routes as Switch, Route, Navigate} from 'react-router-dom'
+import { Main } from './Main'
 
 import { Results } from './Results'
 
@@ -7,7 +8,7 @@ export function Routes() {
   return (
     <div className='p-4'>
       <Switch>
-        <Route exact path='/' element={<Navigate replace to="/search" />} />
+        <Route exact path='/' element={<Main />} />
         {['/search', '/image', '/news', '/video'].map((path, index) => (
           <Route exact path={path} key={index} element={<Results />} />
         ))}
