2302f8f0a03cf0d6fe80219fa02b74741b559960
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-07-10 13:43:33 +0900

    지도 검색 기능 구현

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,22 +1,71 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const { kakao } = window;
 
+
 export const Maps = () => {
+  const [InputText, setInputText] = useState('')
+  const [Place, setPlace] = useState('')
+
+  const onChange = (e) => {
+    setInputText(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setPlace(InputText)
+    setInputText('')
+  }
+
   useEffect(() => {
-    let container = document.getElementById("map");
-
-    let options = {
+    let infowindow = new kakao.maps.InfoWindow({ zIndex: 1 })
+    const container = document.getElementById('myMap')
+    const options = {
       center: new kakao.maps.LatLng(33.450701, 126.570667),
-      level: 3
-    };
+      level: 3,
+    }
+    const map = new kakao.maps.Map(container, options)
 
-    let map = new kakao.maps.Map(container, options);
+    const ps = new kakao.maps.services.Places()
 
-    console.log(map);
-  }, []);
+    ps.keywordSearch(Place, placesSearchCB)
+
+    function placesSearchCB(data, status, pagination) {
+      if (status === kakao.maps.services.Status.OK) {
+        let bounds = new kakao.maps.LatLngBounds()
+
+        for (let i = 0; i < data.length; i++) {
+          displayMarker(data[i])
+          bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x))
+        }
+
+        map.setBounds(bounds)
+      }
+    }
+
+    function displayMarker(place) {
+      let marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place.y, place.x),
+      })
+
+      // 마커에 클릭이벤트를 등록합니다
+      kakao.maps.event.addListener(marker, 'click', function () {
+        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+        infowindow.setContent('<div className="text-2xl p-4">' + place.place_name + '</div>')
+        infowindow.open(map, marker)
+      })
+    }
+  }, [Place])
 
   return (
-    <div id='map' className='h-80'></div>
+    <>
+      <form className="inputForm" onSubmit={handleSubmit}>
+        <input placeholder="검색어를 입력하세요" onChange={onChange} value={InputText} />
+        <button type="submit">검색</button>
+      </form>
+      <div id="myMap" className='h-screen text-black'>
+      </div>
+    </>
   )
 }
