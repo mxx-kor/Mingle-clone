c1164ad68542aaad9be5ae95d160356515aaf363
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-07-20 01:45:28 +0900

    배포 시도

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
         <div className={darkTheme ? 'dark' : ''}>
             <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200 black min-h-screen">
                 <Switch>
-                    <Route exact path='/' element={<Main darkTheme={darkTheme} setDarkTheme={setDarkTheme} />} />
+                    <Route exact path='/mingle-clone' element={<Main darkTheme={darkTheme} setDarkTheme={setDarkTheme} />} />
                     <Route path='/*' element={<Routes darkTheme={darkTheme} setDarkTheme={setDarkTheme} />} />
                     <Route exact path='/maps' element={<Maps />} />
                 </Switch>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export function Navbar({ darkTheme, setDarkTheme }) {
   return (
     <div className='p-5 pb-0 flex flex-wrap sm:justify-between justify-center border-b dark:border-gray-700 border-gray-200'>
       <div className='flex justify-between items-center space-x-5 w-screen'>
-        <Link to='/'>
+        <Link to='/mingle-clone'>
           <h1 className="text-center text-2xl font-bold">
             <span>M</span><span>i</span><span>n</span><span>g</span><span>l</span><span>e</span>
           </h1> 
